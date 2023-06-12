8dbfd37ba716dd3786aa6cabdac94d2288957bb6
Niel Agenbag <niel.agenbag@gmail.com>
2023-06-12 09:18:03 +0200

    Lukraak getalle bygesit

---
+++ lukraakGetalle/lukraakGetalle.py
@@ -0,0 +1,16 @@
+#%%
+# Lukraak getalle in python
+# Van:
+# https://realpython.com/numpy-random-number-generator/
+
+#%%
+
+import numpy as np
+
+#%%
+default_rng = np.random.default_rng()
+default_rng
+
+#%%
+default_rng.random()
+# %%
